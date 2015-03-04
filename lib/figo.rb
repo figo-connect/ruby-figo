520bb67bc4e1c43bf7460835e393003f77f3534f
mduqueoviedo@gmail.com <mduqueoviedo@gmail.com>
2015-03-04 10:43:31 +0100

    Making branch independent from 'send_newsletter' issue

--- lib/figo.rb
+++ lib/figo.rb
@@ -202,10 +202,10 @@ module Figo
     # @param email [String] Email address; It must obey the figo username & password policy
     # @param password [String] New figo Account password; It must obey the figo username & password policy
     # @param language [String] Two-letter code of preferred language
-    # @param send_newsletter [Boolean] This flag indicates whether the user has agreed to be contacted by email -- Not accepted by Figo backend
+    # @param send_newsletter [Boolean] This flag indicates whether the user has agreed to be contacted by email
     # @return [Hash] object with the key `recovery_password` as documented in the figo Connect API specification
     def create_user(name, email, password, language='de', send_newsletter=true)
-        data = { 'name' => name, 'email' => email, 'password' => password, 'language' => language, 'affiliate_client_id' => @client_id}
+        data = { 'name' => name, 'email' => email, 'password' => password, 'send_newsletter' => send_newsletter, 'language' => language, 'affiliate_client_id' => @client_id}
         return query_api("/auth/user", data)
     end
 
