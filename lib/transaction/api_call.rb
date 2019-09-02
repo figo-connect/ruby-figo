49983fc20389390d2fa2fc180fc6edf159adda53
Lauris <lauris@mikamai.com>
2019-09-02 18:27:17 +0200

    edit doc api

--- lib/transaction/api_call.rb
+++ lib/transaction/api_call.rb
@@ -4,6 +4,29 @@ require_relative 'model.rb'
 module Figo
   # Retrieve list of transactions (on all accounts)
   #
+  # @param options [Object]: further optional options
+  #           accounts: comma separated list of account IDs.
+  #           filter (obj) - Can take 4 possible keys:
+  #               - date (ISO date) - Transaction date
+  #               - person (str) - Payer or payee name
+  #               - purpose (str)
+  #               - amount (num)
+  #           sync_id (str): Show only those items that have been created within this synchronization.
+  #           count (int): Limit the number of returned transactions.
+  #           offset (int): Which offset into the result set should be used to determine the
+  #                       first transaction to return (useful in combination with count)
+  #           sort (enum): ASC or DESC
+  #           since (ISO date): Return only transactions after this date based on since_type
+  #           until (ISO date): This parameter can either be a transaction ID or a date. Return only transactions which were booked on or before
+  #           since_type (enum): This parameter defines how the parameter since will be interpreted.
+  #                               Possible values: "booked" "created" "modified"
+  #           types (enum): Comma separated list of transaction types used for filtering.
+  #                         Possible values:"Transfer", "Standing order", "Direct debit", "Salary or rent", "GeldKarte", "Charges or interest"
+  #           cents (bool): If true amounts will be shown in cents, Optional, default: False
+  #           include_pending (bool): This flag indicates whether pending transactions should
+  #                                 be included in the response. Pending transactions are always
+  #                                 included as a complete set, regardless of the `since` parameter.
+  #           include_statistics (bool): Includes statistics on the returned transactionsif true, Default: false.
   # @return [Array] an array of `Transaction` objects, one for each transaction of the user
   def list_transactions(options)
     path =  "/rest/transactions?#{encodeOptions options}"
@@ -13,6 +36,28 @@ module Figo
   # Retrieve list of transactions (on a specific account)
   #
   # @param account_id [String] ID of the account for which to list the transactions
+  # @param options [Object]: further optional options
+  #           accounts: comma separated list of account IDs.
+  #           filter (obj) - Can take 4 possible keys:
+  #               - date (ISO date) - Transaction date
+  #               - person (str) - Payer or payee name
+  #               - purpose (str)
+  #               - amount (num)
+  #           count (int): Limit the number of returned transactions.
+  #           offset (int): Which offset into the result set should be used to determine the
+  #                       first transaction to return (useful in combination with count)
+  #           sort (enum): ASC or DESC
+  #           since (ISO date): Return only transactions after this date based on since_type
+  #           until (ISO date): This parameter can either be a transaction ID or a date. Return only transactions which were booked on or before
+  #           since_type (enum): This parameter defines how the parameter since will be interpreted.
+  #                               Possible values: "booked" "created" "modified"
+  #           types (enum): Comma separated list of transaction types used for filtering.
+  #                         Possible values:"Transfer", "Standing order", "Direct debit", "Salary or rent", "GeldKarte", "Charges or interest"
+  #           cents (bool): If true amounts will be shown in cents, Optional, default: False
+  #           include_pending (bool): This flag indicates whether pending transactions should
+  #                                 be included in the response. Pending transactions are always
+  #                                 included as a complete set, regardless of the `since` parameter.
+  #           include_statistics (bool): Includes statistics on the returned transactionsif true, Default: false.
   # @return [Array] an array of `Transaction` objects, one for each transaction of the user
   def list_transactions_of_account(account_id, options)
     path = "/rest/accounts/#{account_id}/transactions?#{encodeOptions options}"
