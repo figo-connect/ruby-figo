3ecfb8719ef2df19728e768ef7620ef89a6766fd
Ozim <lukasz.ozimek@netguru.com>
2019-09-30 14:26:32 +0200

    Fix sync API endpoint definition

--- lib/sync/api_call.rb
+++ lib/sync/api_call.rb
@@ -15,7 +15,7 @@ module Figo
   # @return [Object] The Sync created with requested params
   def add_sync(access_id, options)
     data = options.delete_if { |_, v| v.nil? }
-    query_api_object Model::Sync, "rest/accesses/#{access_id}/syncs", data, 'POST'
+    query_api_object Model::Sync, "/rest/accesses/#{access_id}/syncs", data, 'POST'
   end
 
   # Get synchronization status
@@ -24,6 +24,6 @@ module Figo
   # @param sync_id [String] Figo ID of the sync to get
   # @return [Object] The Sync created with requested params
   def get_synchronisation_status(access_id, sync_id)
-    query_api_object Model::Sync, "rest/accesses/#{access_id}/syncs/#{sync_id}", 'GET'
+    query_api_object Model::Sync, "/rest/accesses/#{access_id}/syncs/#{sync_id}", 'GET'
   end
 end
