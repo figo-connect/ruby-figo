0343253f70de9f295188f532d512ee9fe02afd9c
Ozim <lukasz.ozimek@netguru.com>
2019-10-04 16:16:14 +0200

    Update catalog API calling

--- lib/catalog/api_call.rb
+++ lib/catalog/api_call.rb
@@ -9,25 +9,25 @@ module Figo
   # List complete catalog (user auth or client auth.)
   # The real catalog contains thousands of entries, so expect the call to cause some traffic and delay.
   #
-  # @param q [String] Two-letter country code. Show only resources within this country. Country code is case-insensitve.
-  # @param country [String] Query for the entire catalog. Will match banks on domestic bank code, BIC, name or figo-ID.
+  # @param country[String] Two-letter country code. Show only resources within this country. Country code is case-sensitve.
+  # @param q [String] Query for the entire catalog. Will match banks on domestic bank code, BIC, name or figo-ID.
   #        Will match services based on name or figo-ID. Only exact matches are returned.
   # @params objects [Enum] "banks" or "services", decide what is included in the api response.
   #         If not specified, it returns both
   # @return [Catalog] modified bank object returned by server
-  def list_complete_catalog(q = nil, country = nil)
+  def list_complete_catalog(country = nil, q = nil)
     options = { q: q, country: country }
     path = "#{@prefix}/catalog"
     query_api_object(Model::Catalog, parameterized_path(path, options), nil, 'GET')
   end
 
-  def list_banks(q = nil, country = nil)
+  def list_banks(country = nil, q = nil)
     options = { q: q, country: country }
     path = '/catalog/banks'
     query_api_object(Model::Bank, parameterized_path(path, options), nil, 'GET', 'banks')
   end
 
-  def list_services(q = nil, country = nil)
+  def list_services(country = nil, q = nil)
     options = { q: q, country: country }
     path = '/catalog/services'
     query_api_object(Model::Service, parameterized_path(path, options), nil, 'GET', 'services')
